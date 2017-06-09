dba2ea05481472fd6e730e4e5196fcfd141bd785
Takayuki Nakata <f.seasons017@gmail.com>
2017-06-09 12:44:59 +0900

    add `ln -s` for `.git-completion.bash`

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 ln -s $HOME/dotfiles/.bash_profile ~/.bash_profile
 ln -s $HOME/dotfiles/.bashrc ~/.bashrc
 ln -s $HOME/dotfiles/.gitconfig ~/.gitconfig
+ln -s $HOME/dotfiles/.git-completion.bash ~/.git-completion.bash
