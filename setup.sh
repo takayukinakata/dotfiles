db5e78dc9d0df9135195ec6ce160bbde1f09b3cd
Takayuki Nakata <f.seasons017@gmail.com>
2017-06-20 14:27:19 +0900

    Update setup.sh for .tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.bash_profile .bashrc .gitconfig .git-completion.bash)
+DOT_FILES=(.bash_profile .bashrc .gitconfig .git-completion.bash .tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
