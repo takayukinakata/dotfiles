2e3214ed067a3f1bb0311fed00b64663a3506c8e
tnakata <f.seasons017@gmail.com>
2017-07-04 14:12:14 +0900

    uninstall magit, term+, term+mux

--- .emacs.d/elpa/term+mux-20140210.2349/term+mux-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "term+mux" "20140210.2349" "term+ terminal multiplexer and session management" '((term+ "0.1") (tab-group "0.1")) :commit "81b60e80cf008472bfd7fad9233af2ef722c208a" :url "http://github.com/tarao/term+-el" :keywords '("terminal" "emulation"))
