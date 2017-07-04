8df7c97fffe37e551aacc28102884b19bbe3c7b9
tnakata <f.seasons017@gmail.com>
2017-07-04 11:20:10 +0900

    Install term+mux

---
+++ .emacs.d/elpa/term+mux-20140210.2349/term+mux-autoloads.el
@@ -0,0 +1,15 @@
+;;; term+mux-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("term+mux.el") (22874 64180 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; term+mux-autoloads.el ends here
