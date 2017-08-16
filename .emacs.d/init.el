2e95ccf92338f5b1ec190e401a6e7bd20ff43563
tnakata <f.seasons017@gmail.com>
2017-08-16 16:03:48 +0900

    improve wgrep

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -166,6 +166,12 @@
 
 ;;; set wgrep
 (require 'wgrep nil t)
+;;; eでwgrepモードにする
+(setf wgrep-enable-key "e")
+;;; wgrep終了時にバッファを保存
+(setq wgrep-auto-save-buffer t)
+;;; read-only bufferにも変更を適用する
+(setq wgrep-change-readonly-file t)
 
 ;;; set gfm-mode
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
