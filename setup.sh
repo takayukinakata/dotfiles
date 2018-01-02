7b5328d29055a67fadbd7eef0319fe69e9a5208c
tnakata <f.seasons017@gmail.com>
2018-01-02 21:50:00 +0900

    Remove `.tmux.conf`

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.bash_profile .bashrc .gitconfig .gitignore_global .git-completion.bash .emacs.d .tmux.conf)
+DOT_FILES=(.bash_profile .bashrc .gitconfig .gitignore_global .git-completion.bash .emacs.d)
 
 for file in ${DOT_FILES[@]}
 do
