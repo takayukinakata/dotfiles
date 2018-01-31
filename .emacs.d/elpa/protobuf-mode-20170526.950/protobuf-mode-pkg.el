41a0210f21691c7eed9d6c1f2dad0dc4a669df91
tnakata <f.seasons017@gmail.com>
2018-01-31 16:55:04 +0900

    Install protobuf-mode

---
+++ .emacs.d/elpa/protobuf-mode-20170526.950/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.950" "major mode for editing protocol buffers." 'nil :commit "80e016e83898b2bd4f197f1d57c7e1453828e846" :keywords '("google" "protobuf" "languages"))
