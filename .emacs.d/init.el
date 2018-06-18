5092f1060202a3f6aaabd9e6ec8790b9b76dc6a5
tnakata <f.seasons017@gmail.com>
2018-06-18 16:02:00 +0900

    Update go-company setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -244,7 +244,6 @@
             (company-mode)
             (helm-gtags-mode)
             (setq company-tooltip-limit 20)                      ; bigger popup window
-            (setq company-idle-delay .3)                         ; decrease delay before autocompletion (point)opup shows
             (setq company-echo-delay 0)                          ; remove annoying blinking
             (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
             (custom-set-faces
