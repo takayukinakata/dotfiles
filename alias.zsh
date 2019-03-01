07a4b94338a0f88b9e59be26dbfadf4a7968f466
tnakata <f.seasons017@gmail.com>
2019-03-01 10:21:38 +0900

    Add alias `fzf` to add default option

--- alias.zsh
+++ alias.zsh
@@ -11,6 +11,8 @@ alias -g B='`g b | fzf --reverse | sed -e "s/^\*[ ]*//g"`'
 alias gtagsupd="gtags -v -i --gtagslabel=pygments --debug --explain"
 # emacs
 alias e='emacs'
+# fzf
+alias fzf='fzf -m --reverse'
 # vagrant
 alias vu='vagrant up'
 alias vh='vagrant halt'
